4406c46cb92cdd4e3c596bbab033668f87fa6a17
Jocatins <jocakhamen@gmail.com>
2021-11-24 10:09:19 +0100

    contextApp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import "./App.css";
+import React from "react";
+import BookForm from "./components/BookForm";
 import BookList from "./components/BookList";
 import Navbar from "./components/Navbar";
-import ThemeToggle from "./components/ThemeToggle";
-import AuthContextProvider from "./contexts/AuthContext";
 import BookContextProvider from "./contexts/BookContext";
-import ThemeContextProvider from "./contexts/ThemeContext";
 
 function App() {
   return (
     <div className="App">
-      <ThemeContextProvider>
-        <AuthContextProvider>
-          <Navbar />
-          <BookContextProvider>
-            <BookList />
-          </BookContextProvider>
-          <ThemeToggle />
-        </AuthContextProvider>
-      </ThemeContextProvider>
+      <BookContextProvider>
+        <Navbar />
+        <BookList />
+        <BookForm />
+      </BookContextProvider>
     </div>
   );
 }
